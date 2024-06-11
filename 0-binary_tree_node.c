0294794a1815b4e83500380efe62bca936eb6c36
abangayoma <abangayoma@gmail.com>
2024-06-11 09:08:44 -0700

    corrected most files

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	// allocate memory to the new node created
 	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 	//check if list is empty;
-	if(new_node = NULL)
+	if(new_node == NULL)
 		return(NULL);
 	new_node->n = value;
 	new_node->right = NULL;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,11 +14,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent,value);
-	if (new_node = NULL)
+	if (new_node == NULL)
 		return (NULL);
 	if (parent->left != NULL)
 	{
-		new->left = parent->left;
+		new_node->left = parent->left;
 		parent->left->left = new_node;
 	}
 	parent->left = new_node;
